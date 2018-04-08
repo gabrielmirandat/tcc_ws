0cc947621d6aabb381e6b00b6297f70bf9e8a673
gabrielmirandat <gabrielmirandatt@gmail.com>
2018-04-08 12:14:59 -0300

    Publishing images grayscale compressed

--- catkin_ws/src/resources/nodes/src/camera.cc
+++ catkin_ws/src/resources/nodes/src/camera.cc
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
@@ -19,15 +20,16 @@ int main(int argc, char** argv)
   cv::VideoCapture cap(video_source);
   // Check if video device can be opened with the given index
   if(!cap.isOpened()) return 1;
-  cv::Mat frame;
+  cv::Mat frame, frame_gray;
   sensor_msgs::ImagePtr msg;
 
   ros::Rate loop_rate(50);
   while (nh.ok()) {
     cap >> frame;
-    // Check if grabbed frame is actually full with some content
+
     if(!frame.empty()) {
-      msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", frame).toImageMsg();
+      cv::cvtColor( frame, frame_gray, CV_BGR2GRAY );
+      msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", frame_gray).toImageMsg();
       pub.publish(msg);
       cv::waitKey(1);
     }
